905db931d0e1c700adf14023f36eaef2177fb27c
kalyanvarma897 <kalyanraj897@gmail.com>
2025-05-12 17:50:04 +0530

    Updated pom.xml and email tests, added Redis config and tests

--- src/test/java/com/kalyan/journalApp/service/EmailServiceTests.java
+++ src/test/java/com/kalyan/journalApp/service/EmailServiceTests.java
@@ -1,5 +1,6 @@
 package com.kalyan.journalApp.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ public class EmailServiceTests {
     @Autowired
     private EmailService emailService;
 
+    @Disabled
     @Test
     public void testSendMail(){
         emailService.sendEmail("yaswanthvarma897@gmail.com",
