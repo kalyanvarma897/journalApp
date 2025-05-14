3c8fd43ccad8ccc2944001eb7716cde2be815385
kalyanvarma897 <kalyanraj897@gmail.com>
2025-05-14 15:21:37 +0530

    Add sentiment consumer service and model, update scheduler and pom

--- src/main/java/com/kalyan/journalApp/scheduler/UserScheduler.java
+++ src/main/java/com/kalyan/journalApp/scheduler/UserScheduler.java
@@ -4,10 +4,13 @@ import com.kalyan.journalApp.cache.AppCache;
 import com.kalyan.journalApp.entity.JournalEntry;
 import com.kalyan.journalApp.entity.User;
 import com.kalyan.journalApp.enums.Sentiment;
+import com.kalyan.journalApp.model.SentimentData;
 import com.kalyan.journalApp.repository.UserRepositoryImpl;
 import com.kalyan.journalApp.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -16,7 +19,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+
+@Component
 public class UserScheduler {
+
 
     @Autowired
     private EmailService emailService;
@@ -24,19 +30,18 @@ public class UserScheduler {
     @Autowired
     private UserRepositoryImpl userRepository;
 
-
-
-
-
     @Autowired
     private AppCache appCache;
 
-    @Scheduled(cron = "0 9 * * SUN")
-    public void fetchUsersAndSendSaMail(){
+    @Autowired
+    private KafkaTemplate<String, SentimentData> kafkaTemplate;
+
+    @Scheduled(cron = "0 0 9 * * SUN")
+    public void fetchUsersAndSendSaMail() {
         List<User> users = userRepository.getUserForSA();
-        for(User user:users){
-            List<JournalEntry> journalEntries =user.getJournalEntries();
-            List<Sentiment> sentiments= journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
+        for (User user : users) {
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
             Map<Sentiment, Integer> sentimentCounts = new HashMap<>();
             for (Sentiment sentiment : sentiments) {
                 if (sentiment != null)
@@ -50,18 +55,20 @@ public class UserScheduler {
                     mostFrequentSentiment = entry.getKey();
                 }
             }
-            if(mostFrequentSentiment!=null){
-                emailService.sendEmail(user.getEmail(),"Sentiment for last 7 days",mostFrequentSentiment.toString());
+            if (mostFrequentSentiment != null) {
+                SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days " + mostFrequentSentiment).build();
+                try{
+                    kafkaTemplate.send("weekly-sentiments", sentimentData.getEmail(), sentimentData);
+                }catch (Exception e){
+                    emailService.sendEmail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+                }
             }
-
         }
-
-
-
     }
 
     @Scheduled(cron = "0 0/10 * ? * *")
     public void clearAppCache() {
         appCache.init();
     }
+
 }
